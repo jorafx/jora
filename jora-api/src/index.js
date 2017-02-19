e0d13ce038eae9a9c4d9075db19ea0bf7f00a128
Marcus Hammarberg <marcusoft.net@gmail.com>
2017-02-19 21:19:29 +0100

    Added standard linting

--- jora-api/src/index.js
+++ jora-api/src/index.js
@@ -1,30 +1,27 @@
-import http from 'http';
-import _ from 'koa-route';
-import koa from 'koa';
+import _ from 'koa-route'
+import koa from 'koa'
 
-import * as projects from './projectController';
-import * as tasks from './taskController';
+import * as projects from './projectController'
+import * as tasks from './taskController'
 
-const app = koa();
+const app = koa()
 
-app.use(_.post('/projects', projects.create));
-app.use(_.get('/projects/findByTag', projects.findByTag));
+app.use(_.post('/projects', projects.create))
+app.use(_.get('/projects/findByTag', projects.findByTag))
 
-app.use(_.get('/project/:projectId', projects.findById));
-app.use(_.post('/project/:projectId', projects.update));
-app.use(_.put('/project/:projectId', projects.update));
-app.use(_.delete('/project/:projectId', projects.remove));
+app.use(_.get('/project/:projectId', projects.findById))
+app.use(_.post('/project/:projectId', projects.update))
+app.use(_.put('/project/:projectId', projects.update))
+app.use(_.delete('/project/:projectId', projects.remove))
 
-app.use(_.post('/tasks', tasks.create));
-app.use(_.get('/tasks/findByTag', tasks.findByTag));
-app.use(_.get('/tasks/findByProject', tasks.findByProject));
+app.use(_.post('/tasks', tasks.create))
+app.use(_.get('/tasks/findByTag', tasks.findByTag))
+app.use(_.get('/tasks/findByProject', tasks.findByProject))
 
-app.use(_.get('/tasks/:tasksId', tasks.findById));
-app.use(_.post('/tasks/:tasksId', tasks.update));
-app.use(_.put('/tasks/:tasksId', tasks.update));
-app.use(_.delete('/tasks/:tasksId', tasks.remove));
+app.use(_.get('/tasks/:tasksId', tasks.findById))
+app.use(_.post('/tasks/:tasksId', tasks.update))
+app.use(_.put('/tasks/:tasksId', tasks.update))
+app.use(_.delete('/tasks/:tasksId', tasks.remove))
 
-
-
-app.listen(3000);
-console.log('listening on port 3000');
+app.listen(3000)
+console.log('listening on port 3000')
--- jora-api/src/projectController.js
+++ jora-api/src/projectController.js
@@ -1,8 +1,7 @@
+let create = function *() { this.body = 'create called!' }
+let findByTag = function *() { this.body = 'findByTag called' }
+let findById = function *(id) { this.body = `findById called with ${id}` }
+let update = function *(id) { this.body = `update called with ${id}` }
+let remove = function *(id) { this.body = `remove called with ${id}` }
 
-let create = function *() { this.body = "create called!" };
-let findByTag = function *() { this.body = "findByTag called" };
-let findById = function *(id) { this.body = `findById called with ${id}`; }
-let update = function *(id) { this.body = `update called with ${id}`; }
-let remove = function *(id) { this.body = `remove called with ${id}`; }
-
-export {create, findByTag, findById, update, remove };
+export {create, findByTag, findById, update, remove}
