f932d21711ea143c5cc778d17b676b24f7222495
Marcus Hammarberg <marcusoft.net@gmail.com>
2017-02-20 20:36:13 +0100

    Linted and clean

--- jora-api/src/projectController.test.js
+++ jora-api/src/projectController.test.js
@@ -1,11 +1,11 @@
 import 'babel-polyfill'
-import should from 'should'
+import {describe, it} from 'mocha'
 import * as projects from './projectController'
 
-describe("project controller", function () {
-	it("has a function for create", () => { projects.create.should.not.be.null; })
-	it("has a function for findByTag", () => { projects.findByTag.should.not.be.null; })
-	it("has a function for findById", () => { projects.findById.should.not.be.null; })
-	it("has a function for update", () => { projects.update.should.not.be.null; })
-	it("has a function for remove", () => { projects.remove.should.not.be.null; })
+describe('project controller', function () {
+  it('has a function for create', () => { projects.create.should.not.be.null })
+  it('has a function for findByTag', () => { projects.findByTag.should.not.be.null })
+  it('has a function for findById', () => { projects.findById.should.not.be.null })
+  it('has a function for update', () => { projects.update.should.not.be.null })
+  it('has a function for remove', () => { projects.remove.should.not.be.null })
 })
